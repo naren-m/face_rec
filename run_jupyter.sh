34ebd9ed0c827977313650091adca3b5a26ef10f
Naren Mudivarthy <nmudivar@cisco.com>
2020-01-06 17:16:34 -0800

    Not sure what this is

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -14,5 +14,6 @@
 # limitations under the License.
 # ==============================================================================
 
+source /Users/$USER/.virtualenvs/dl/bin/activate
 
 jupyter notebook --allow-root "$@" 
